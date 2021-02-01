35fc74dec23bb2b1226ea37aa4a562a9eacb6b3a
Piotr Tajanowicz <piotr.tajanowicz@ibm.com>
2021-02-01 13:33:03 +0100

    upd

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,17 @@ use App\Http\Controllers\Index;
 | contains the "web" middleware group. Now create something great!
 |
 */
+// Overtime Requests
+Route::prefix('request')
+    ->middleware('auth')
+    ->name('request.')
+    ->group(function () {
+
+        // Show the form for editing the specified resource.
+        Route::get('edit/{overtimeRequest}', [OvertimeRequests::class, 'edit'])
+            ->name('edit');
+    
+    });
 
 // Vue
 Route::get('/{any}', [Index::class, 'index'])->where('any', '.*');
